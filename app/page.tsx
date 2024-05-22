14f30ef6e302581ab80601d17aa18faee64d0457
Arsalan Hussain <arsalanmhussain32@gmail.com>
2024-05-22 12:40:23 +0500

    add sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import { Box, Title } from '@mantine/core';
 import { HeroBullets } from '@/components/Hero/hero';
 
 export default function HomePage() {
   return (
     <>
       <HeroBullets />
+      <Section title="About" />
+      <Section title="Experience" />
+      <Section title="Projects" />
+      <Section title="Skills" />
+      <Section title="Contact me" />
     </>
+  );
+}
+
+function Section({ title }: { title: string }) {
+  return (
+    <Box h="100vh" component="section" id={title.toLowerCase()}>
+      <Title ta="center">{title}</Title>
+    </Box>
   );
 }
